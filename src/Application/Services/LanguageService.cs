216ee700859ba409a34bf6db17bb878ce914c8d0
berkaybyr <77205615+berkaybyr@users.noreply.github.com>
2023-02-03 17:12:14 +0300

    language controller endpoints added

--- src/Application/Services/LanguageService.cs
+++ src/Application/Services/LanguageService.cs
@@ -12,19 +12,42 @@ namespace ECom.Application.Services
 	public interface ILanguageService 
 	{
 		public List<Language> GetLanguages();
+		public ResultData<Language> GetLanguageById(int id);
+
+		public Result EnableOrDisable(int id);
+
+		
 	}
 	public class LanguageService : ILanguageService
 	{
 		private readonly IEfEntityRepository<Language> _languageRepo;
 
-		public LanguageService(
-			IEfEntityRepository<Language> languageRepo)
+		public LanguageService(IEfEntityRepository<Language> languageRepo)
 		{
 			this._languageRepo = languageRepo;
 		}
 		public List<Language> GetLanguages()
 		{
 			return _languageRepo.GetList();
+		}
+		public ResultData<Language> GetLanguageById(int id)
+		{
+			var lang  = _languageRepo.Find(id);
+			if(lang is null)
+			{
+				return ResultData<Language>.Error(1, ErrCode.NotFound, "Language");
+			}
+			return ResultData<Language>.Success(lang);
+		}
+
+		public Result EnableOrDisable(int id)
+		{
+			var language = _languageRepo.Find(id);
+			if (language is null) return Result.Error(1, ErrCode.NotFound,"Language");
+			language.IsValid = !language.IsValid;
+			var res = _languageRepo.Update(language);
+			if(!res) return Result.Error(1,ErrCode.DbErrorInternal,"Language","EnableOrDisable");
+			return Result.Success("Updated");
 		}
 	}
 }
--- src/ECom.WebApi/Controllers/UserControllers/LanguageController.cs
+++ src/ECom.WebApi/Controllers/UserControllers/LanguageController.cs
@@ -15,6 +15,32 @@ namespace ECom.WebApi.Controllers.UserControllers
 			var res = _languageService.GetLanguages();
 			return Ok(res);
 		}
-		
+
+
+		[HttpPut]
+		public IActionResult EnableOrDisable(int id)
+		{
+			var res = _languageService.EnableOrDisable(id);
+			if (!res.IsSuccess)
+			{
+				logger.Warn(res.Rv, res.ErrorCode, res.Parameters);
+				return BadRequest(res);
+			}
+			logger.Info("Language EnabledOrDisabled: " + id);
+			return Ok(res);
+		}
+
+		[HttpGet]
+		public IActionResult Get(int id)
+		{
+			var res = _languageService.GetLanguageById(id);
+			if (!res.IsSuccess)
+			{
+				logger.Warn(res.Rv, res.ErrorCode, res.Parameters,);
+				return BadRequest(res);
+			}
+			logger.Info(res.Data.ToJsonString());
+			return Ok(res);
+		}
 	}
 }
